d0a32878c6d41f17d220d7c96a3ada71fd8cd4fa
Ethan Godley <62864699+ethangodley@users.noreply.github.com>
2022-06-02 20:58:07 +0800

    Added generatPW function to JS file

--- script.js
+++ script.js
@@ -24,4 +24,39 @@ function selectPWLength() {
     prompt("Error, please select a valid input");
     return selectPWLength();
   }
+  //User selects what characters to choose from and creates random password using those characters
+  function generatePW() {
+    // calls upon passwordLength function and stores password length in local variable
+    const passwordLength = selectPWLength();
+    window.confirm("Please select characters to include");
+    // asks user which characters to include and stores answers in boolean format
+    const lower = window.confirm("Include lower-case?");
+    const upper = window.confirm("Include upper-case?");
+    const number = window.confirm("Include numbers?");
+    const special = window.confirm("Include special?");
+  
+    // sets character selection string to empty
+    let charSelections = "";
+    // uses if statement to check desired character inclusions and adds desired character inclusions to string
+    if(lower) {
+      charSelections += "abcdefghi";
+    }
+    if(upper) {
+      charSelections += "ABCDEFGHI"
+    }
+    if(number) {
+      charSelections += "123456789"
+    }
+    if(special) {
+      charSelections += "!@#$%^&*?"
+    }
+    // initializes password variable to empty string
+    let password = "";
+    // fills password string with random characters of desired types until desired PW length reached
+    for ( let i = 0; i < passwordLength; i++ ) {
+      password += charSelections.charAt(Math.floor(Math.random() * charSelections.length));
+    }
+    // returns completed random password
+    return password;
+  }
 }
