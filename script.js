cc544827e90b24f81937581d7f3994e8896cfe49
Ethan Godley <62864699+ethangodley@users.noreply.github.com>
2022-06-02 21:16:53 +0800

    Fixed logic errors within code and conducted final test

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function selectPWLength() {
   //if statement validates password length to be a number between 8 and 128 inclusive
   if(passwordLength < 8 || passwordLength > 128){
     // if number is outside length range display error message and start function again
-    prompt("Error, please select a length from 8 - 128");
+    confirm("Error, please select a length from 8 - 128");
     return selectPWLength();
   }
   else if(passwordLength >= 8 && passwordLength <= 128) {
@@ -21,51 +21,50 @@ function selectPWLength() {
   }
   else {
     // if foreign value added in field, display error message and restart function
-    prompt("Error, please select a valid input");
+    confirm("Error, please select a valid input");
     return selectPWLength();
   }
-  //User selects what characters to choose from and creates random password using those characters
-  function generatePW() {
-    // calls upon passwordLength function and stores password length in local variable
-    const passwordLength = selectPWLength();
-    window.confirm("Please select characters to include");
-    // asks user which characters to include and stores answers in boolean format
-    const lower = window.confirm("Include lower-case?");
-    const upper = window.confirm("Include upper-case?");
-    const number = window.confirm("Include numbers?");
-    const special = window.confirm("Include special?");
-  
-    // sets character selection string to empty
-    let charSelections = "";
-    // uses if statement to check desired character inclusions and adds desired character inclusions to string
-    if(lower) {
-      charSelections += "abcdefghi";
-    }
-    if(upper) {
-      charSelections += "ABCDEFGHI"
-    }
-    if(number) {
-      charSelections += "123456789"
-    }
-    if(special) {
-      charSelections += "!@#$%^&*?"
-    }
-    // initializes password variable to empty string
-    let password = "";
-    // fills password string with random characters of desired types until desired PW length reached
-    for ( let i = 0; i < passwordLength; i++ ) {
-      password += charSelections.charAt(Math.floor(Math.random() * charSelections.length));
-    }
-    // returns completed random password
-    return password;
+}
+//User selects what characters to choose from
+function generatePW() {
+  // calls upon passwordLength function and stores password length in local variable
+  const passwordLength = selectPWLength();
+  window.confirm("Please select characters to include");
+  // asks user which characters to include and stores answers in boolean format
+  const lower = window.confirm("Include lower-case?");
+  const upper = window.confirm("Include upper-case?");
+  const number = window.confirm("Include numbers?");
+  const special = window.confirm("Include special?");
+
+  // sets character selection string to empty
+  let charSelections = "";
+  // uses if statement to check desired character inclusions and adds desired character inclusions to string
+  if(lower) {
+    charSelections += "abcdefghi";
   }
-  // function displays generated password to screen in allocated box
-  function writePassword() {
-    // calls upon generatePW function
-    const password = generatePW();
-    //links passwordText variable to password id in HTML file
-    const passwordText = document.querySelector("#password");
-  // sets value of passwordText to equal completed random password which will now display on screen
-    passwordText.value = password;
-  }  
+  if(upper) {
+    charSelections += "ABCDEFGHI"
+  }
+  if(number) {
+    charSelections += "123456789"
+  }
+  if(special) {
+    charSelections += "!@#$%^&*?"
+  }
+  // initializes password variable to empty string
+  let password = "";
+  // fills password string with random characters of desired types until desired PW length reached
+  for ( let i = 0; i < passwordLength; i++ ) {
+    password += charSelections.charAt(Math.floor(Math.random() * charSelections.length));
+  }
+  // returns completed random password
+  return password;
+}
+function writePassword() {
+  // calls upon generatePW function
+  const password = generatePW();
+  //links passwordText variable to password id in HTML file
+  const passwordText = document.querySelector("#password");
+// sets value of passwordText to equal completed random password which will now display on screen
+  passwordText.value = password;
 }
